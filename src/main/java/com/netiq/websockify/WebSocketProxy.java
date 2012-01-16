233560efdff1f3231f48a81a404b146baedf726e
Jarrod Ribble <jribble@netiq.com>
2012-01-16 16:38:13 -0700

    Java websockify server

---
+++ src/main/java/com/netiq/websockify/WebSocketProxy.java
@@ -0,0 +1,191 @@
+package com.netiq.websockify;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.netiq.websocket.Base64;
+import com.netiq.websocket.Draft;
+import com.netiq.websocket.Framedata;
+import com.netiq.websocket.WebSocket;
+import com.netiq.websocket.WebSocketAdapter;
+
+public class WebSocketProxy extends WebSocketAdapter implements Runnable {
+	public static int BUFFER_SIZE = 65536;
+
+	protected boolean debug;
+	protected WebSocket webSocket;
+	protected SelectionKey webSocketKey;
+	protected SocketChannel targetServer;
+	protected SelectionKey targetServerKey;
+	protected Selector selector;
+	protected LinkedBlockingQueue<ByteBuffer> toTargetServerQueue = new LinkedBlockingQueue<ByteBuffer>();
+	protected LinkedBlockingQueue<ByteBuffer> toWebSocketQueue = new LinkedBlockingQueue<ByteBuffer>();
+	protected LinkedBlockingQueue<ByteBuffer> toWebSocketFrameQueue = new LinkedBlockingQueue<ByteBuffer>();
+
+	public WebSocketProxy(SocketChannel client, String host, int port, boolean debug) throws IOException {
+		this.debug = debug;
+		WebSocket webSocket = new WebSocket (client, new LinkedBlockingQueue<ByteBuffer>(), this, Integer.MAX_VALUE);
+		this.webSocket = webSocket;
+		targetServer = SocketChannel.open(new InetSocketAddress(host, port));
+		targetServer.configureBlocking(false);
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+	
+	public WebSocketProxy(SocketChannel client, String host, int port) throws IOException
+	{
+		this(client, host, port, false);
+	}
+
+	@Override
+	public void onMessage(WebSocket msgSocket, String message) {
+		try {
+			if(debug) System.out.print("}");
+			byte[] buf = Base64.decode(message);
+			ByteBuffer buffer = ByteBuffer.wrap(buf);
+			//buffer.position(buffer.capacity());
+			//System.out.println("Read from web socket " + buffer.position() + " bytes:" + message + "\n" + debugString(buffer));
+			toTargetServerQueue.add(buffer);
+			
+		} catch (IOException e) {
+			//System.out.println(e.getMessage());
+		}
+
+	}
+
+	// Runnable IMPLEMENTATION /////////////////////////////////////////////////
+	public void run() {
+		try {
+
+			selector = Selector.open();
+			targetServerKey = targetServer.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+			webSocketKey = webSocket.socketChannel().register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, webSocket);
+		} catch (IOException ex) {
+			return;
+		}
+
+		while (true) {
+			try {
+				selector.select();
+				
+				if(!targetServerKey.isValid() || !webSocketKey.isValid()){
+					targetServer.close();
+					webSocket.close();
+					break;
+				}
+
+				if(!webSocket.isHandshakeComplete()){
+					//System.out.println("Handling websocket handshake.");
+					if (webSocketKey.isReadable()) {
+						webSocket.handleRead();
+					}
+				}
+				else{
+					if (targetServerKey.isWritable() && !toTargetServerQueue.isEmpty()) {
+						ByteBuffer buffer = toTargetServerQueue.element();
+						//System.out.println("Writing to target server " + buffer.position() + " bytes:" + debugString(buffer));
+						//buffer.flip();
+						if(debug) System.out.print(">");
+						targetServer.write(buffer);
+						//System.out.println(written + " bytes written.");
+						if(buffer.hasRemaining()) System.out.print(".");
+						if (!buffer.hasRemaining())
+							toTargetServerQueue.remove();
+					}
+	
+					if (targetServerKey.isReadable()) {
+						ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+						targetServer.read(buffer);
+						if(debug) System.out.print("{");
+						//System.out.println("Read from target server " + buffer.position() + " bytes:" + debugString(buffer));
+						toWebSocketQueue.add(buffer);
+					}
+					
+					while(!toWebSocketQueue.isEmpty()){
+						//webSocket.handleWrite();
+						ByteBuffer buffer = toWebSocketQueue.remove();
+						String toSend = Base64.encodeBytes(buffer.array(), 0, buffer.position());
+						//System.out.println("Writing to web socket " + buffer.position() + " bytes:" + debugString(buffer) + "\n" + toSend);
+						//webSocket.send(toSend);
+						
+						Draft draft = webSocket.getDraft();
+					    List<Framedata> frames = draft.createFrames ( toSend , false );
+					    if( !frames.isEmpty () ){
+						    for( Framedata f : frames ){
+						    	ByteBuffer b = draft.createBinaryFrame ( f );
+						    	toWebSocketFrameQueue.add(b);
+						    }
+					    }						
+					}
+	
+					if (webSocketKey.isWritable() && !toWebSocketFrameQueue.isEmpty()) {
+					    ByteBuffer buffer = toWebSocketFrameQueue.peek();
+					    webSocket.socketChannel().write(buffer);
+					    if(debug) System.out.print("<");
+					    if(!buffer.hasRemaining()) toWebSocketFrameQueue.remove();
+					    else if(debug) System.out.print(".");
+					}
+	
+					if (webSocketKey.isReadable()) {
+						webSocket.handleRead();
+					}
+				}
+
+			} catch (IOException ex) {
+				if (targetServerKey != null)
+					targetServerKey.cancel();
+				if (webSocketKey != null)
+					webSocketKey.cancel();
+				ex.printStackTrace();
+			} catch (RuntimeException ex) {
+				ex.printStackTrace();
+			} catch (NoSuchAlgorithmException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		// System.err.println("WebSocketServer thread ended!");
+	}
+
+	static final byte[] HEX_CHAR_TABLE = { (byte) '0', (byte) '1', (byte) '2',
+			(byte) '3', (byte) '4', (byte) '5', (byte) '6', (byte) '7',
+			(byte) '8', (byte) '9', (byte) 'a', (byte) 'b', (byte) 'c',
+			(byte) 'd', (byte) 'e', (byte) 'f' };
+
+	public static String getHexString(byte[] raw)
+			throws UnsupportedEncodingException {
+		byte[] hex = new byte[2 * raw.length];
+		int index = 0;
+
+		for (byte b : raw) {
+			int v = b & 0xFF;
+			hex[index++] = HEX_CHAR_TABLE[v >>> 4];
+			hex[index++] = HEX_CHAR_TABLE[v & 0xF];
+		}
+		return new String(hex, "ASCII");
+	}
+
+	String debugString(ByteBuffer buffer) {
+		String fullstr = new String(buffer.array(), 0, buffer.position());
+		String str = fullstr.replaceAll("\\p{Cntrl}", "");
+		if(str.length() < fullstr.length()){
+			try{
+				String hexString = getHexString(Arrays.copyOfRange(buffer.array(), 0, buffer.position()));
+				str = str + "\n" + hexString;
+			}
+			catch(Exception e){
+				str = str + "\n" + e.getMessage();
+			}
+		}
+		return str;
+	}
+}
---
+++ src/main/java/com/netiq/websockify/WebsockifyServer.java
@@ -0,0 +1,127 @@
+package com.netiq.websockify;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class WebsockifyServer implements Runnable {
+	/**
+	 * The port number that this WebSocket server should listen on. Default is
+	 * WebSocket.DEFAULT_PORT.
+	 */
+	protected int port;
+	/**
+	 * The socket channel for this WebSocket server.
+	 */
+	protected ServerSocketChannel server;
+	/**
+	 * The 'Selector' used to get event keys from the underlying socket.
+	 */
+	protected Selector selector;
+
+	protected String targetHost;
+	protected int targetPort;
+
+	public WebsockifyServer(int port, String targetHost, int targetPort) {
+		this.port = port;
+		this.targetHost = targetHost;
+		this.targetPort = targetPort;
+	}
+
+	/**
+	 * Starts the server thread that binds to the currently set port number and
+	 * listeners for WebSocket connection requests.
+	 */
+	public void start() {
+		(new Thread(this)).start();
+	}
+
+	/**
+	 * Closes all connected clients sockets, then closes the underlying
+	 * ServerSocketChannel, effectively killing the server socket thread and
+	 * freeing the port the server was bound to.
+	 * 
+	 * @throws IOException
+	 *             When socket related I/O errors occur.
+	 */
+	public void stop() throws IOException {
+		// for (WebSocket ws : connections) {
+		// ws.close();
+		// }
+		this.server.close();
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	// Runnable IMPLEMENTATION /////////////////////////////////////////////////
+	public void run() {
+		try {
+			server = ServerSocketChannel.open();
+			server.configureBlocking(false);
+			server.socket().bind(new java.net.InetSocketAddress(port));
+
+			selector = Selector.open();
+			server.register(selector, server.validOps());
+		} catch (IOException ex) {
+			// onError(ex);
+			return;
+		}
+
+		while (true) {
+			SelectionKey key = null;
+			try {
+				selector.select();
+				Set<SelectionKey> keys = selector.selectedKeys();
+				Iterator<SelectionKey> i = keys.iterator();
+
+				while (i.hasNext()) {
+					key = i.next();
+
+					// Remove the current key
+					i.remove();
+
+					// if isAcceptable == true
+					// then a client required a connection
+					if (key.isAcceptable()) {
+						SocketChannel client = server.accept();
+						client.configureBlocking(false);
+						new WebSocketProxy(client, targetHost, targetPort);
+					}
+				}
+			} catch (IOException ex) {
+				if (key != null)
+					key.cancel();
+				// onError(ex);
+			} catch (RuntimeException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		// System.err.println("WebSocketServer thread ended!");
+	}
+
+	public static void main(String[] args) {
+		try {
+			if(args.length != 3){
+				System.out.println("Usage: websockify [port] [target host] [target port]");
+			}
+			int port = Integer.parseInt(args[0]);
+			String targetHost = args[1];
+			int targetPort = Integer.parseInt(args[2]);
+			
+			WebsockifyServer s = new WebsockifyServer(port, targetHost, targetPort);
+			s.start();
+			System.out.println("Websockify Server started on port: " + s.getPort());
+		}
+		catch(Exception e) {
+			System.out.println( e.getMessage() );
+		}
+	}
+
+}
